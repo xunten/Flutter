9cd4768d034bcafaf6ed23d356a8800a3dde5a59
Trần Xuân Tiến <tranxuantienvtqs@gmail.com>
2024-10-07 19:44:25 +0700

    lab_4

---
+++ Lab4_Flutter/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: const Text('Ask Me Anything'),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: Ball(),
+        ),
+      ),
+    );
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    ballNumber = Random().nextInt(4) + 1;
+                  });
+                },
+                child: Image.asset('images/ball$ballNumber.png')),
+          ),
+        ],
+      ),
+    );
+  }
+}
